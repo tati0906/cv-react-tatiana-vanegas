97dda6e5afa42c982e8d7fcdeea8141e48206a92
tati0906 <tatianavanegas2006@gmail.com>
2025-06-16 16:24:16 -0500

    const [mostrar, setMostrar]

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import CabeceraCV from "./components/CabeceraCV.jsx";
-import Perfil from "./components/Perfil.jsx";
-import Educacion from "./components/Educacion.jsx";
-import Experiencia from "./components/Experiencia.jsx";
-import StackTecnologias from "./components/StackTecnologias.jsx";
-import Proyectos from "./components/Proyectos.jsx";
-import Habilidades from "./components/Habilidades.jsx";
+import React, { useState } from "react";
 import {
   CabeceraCV as cabeceraData,
   Perfil as perfilData,
   Educacion as educacionData,
   Experiencia as experienciaData,
-  stackTecnologias,
+  stackTecnologias as tecnologiasIniciales,
   Proyectos as proyectosData,
-  Habilidades as habilidadesData
+  Habilidades as habilidadesData,
 } from "./data/cvData";
 
+import CabeceraCV from "./components/CabeceraCV";
+import Perfil from "./components/Perfil";
+import Educacion from "./components/Educacion";
+import Experiencia from "./components/Experiencia";
+import StackTecnologias from "./components/StackTecnologias";
+import Proyectos from "./components/Proyectos";
+import ToggleHabilidades from "./components/ToggleHabilidades";
+import FormularioTecnologia from "./components/FormularioTecnologia";
+
 function App() {
+  const [tecnologias, setTecnologias] = useState(tecnologiasIniciales);
+
+  const agregarTecnologia = (nueva) => {
+    setTecnologias((prev) => [...prev, nueva]);
+  };
+
   return (
-    <div>
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
       <CabeceraCV nombre={cabeceraData.nombre} profesion={cabeceraData.profesion} />
       <Perfil texto={perfilData.texto} />
       <Educacion estudios={educacionData} />
       <Experiencia trabajos={experienciaData} />
-      <StackTecnologias tecnologias={stackTecnologias} />
+      <StackTecnologias tecnologias={tecnologias} />
+      <FormularioTecnologia onAgregar={agregarTecnologia} />
+      <ToggleHabilidades habilidades={habilidadesData} />
       <Proyectos proyectos={proyectosData} />
-      <Habilidades habilidades={habilidadesData} />
     </div>
   );
 }
