974f30a52448ff29bd0ceec86ba857c6b2c50ecf
tati0906 <tatianavanegas2006@gmail.com>
2025-06-16 16:25:13 -0500

    const [mostrar, setMostrar]

--- src/components/FormularioTecnologia.jsx
+++ src/components/FormularioTecnologia.jsx
@@ -1,5 +1,5 @@
 // src/components/FormularioTecnologia.jsx
-import React, { useState } from "react";
+import React, { useState } from "react.";
 
 export default function FormularioTecnologia({ onAgregar }) {
   const [nombre, setNombre] = useState("");
